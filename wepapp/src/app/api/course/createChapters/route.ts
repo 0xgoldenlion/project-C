ccf75600fe4a9f09c7d42188ace90b43a711aa9f
malawad <100777093+0xgoldenlion@users.noreply.github.com>
2023-10-20 02:58:49 +0300

    made pic

--- wepapp/src/app/api/course/createChapters/route.ts
+++ wepapp/src/app/api/course/createChapters/route.ts
@@ -1,4 +1,5 @@
 import { strict_output } from "@/lib/gpt";
+import { getUnsplashImage } from "@/lib/unsplash";
 import { createChaptersSchema } from "@/validators/course";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
@@ -6,33 +7,50 @@ import { ZodError } from "zod";
 export async function POST(req: Request, res: Response) {
     try {
         const body = await req.json()
-        const {title, units} = createChaptersSchema.parse(body)
+        const { title, units } = createChaptersSchema.parse(body)
 
         type outputUnits = {
             title: string;
             chapters: {
-              youtube_search_query: string;
-              chapter_title: string;
+                youtube_search_query: string;
+                chapter_title: string;
             }[];
-          }[];
+        }[];
 
-          let output_units: outputUnits = await strict_output(
+        let output_units: outputUnits = await strict_output(
             "You are an AI capable of curating course content, coming up with relevant chapter titles, and finding relevant youtube videos for each chapter",
             new Array(units.length).fill(
-              `It is your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educationalvideo for each chapter. Each query should give an educational informative course in youtube.`
+                `It is your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educationalvideo for each chapter. Each query should give an educational informative course in youtube.`
             ),
             {
-              title: "title of the unit",
-              chapters:
-                "an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the JSON object",
+                title: "title of the unit",
+                chapters:
+                    "an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the JSON object",
             }
-          );
+        );
+
+        const imageSearchTerm = await strict_output(
+            "you are an AI capable of finding the most relevant image for a course",
+            `Please provide a good image search term for the title of a course about ${title}. This search term will be fed into the unsplash API, so make sure it is a good search term that will return good results`,
+            {
+                image_search_term: "a good search term for the title of the course",
+            }
+        );
+
+        const course_image = await getUnsplashImage(
+            imageSearchTerm.image_search_term
+        );
+
+
+
+
+        console.log("output ", course_image)
         console.log("output ", output_units)
-          return NextResponse.json(output_units)
-        
+        return NextResponse.json(output_units, imageSearchTerm)
+
     } catch (error) {
         if (error instanceof ZodError) {
-            return new NextResponse("invalid body", {status:400})
+            return new NextResponse("invalid body", { status: 400 })
         }
     }
 }
